5499cc7c0d9d9c5cbe5c610a26a5cca3a7e24f28
Michel Thomsen <michel.thomsen@thomsen-online.de>
2019-02-27 00:50:57 +0100

    apply implemetiert

--- fortprog-prak-skeleton/src/Substitutionen.hs
+++ fortprog-prak-skeleton/src/Substitutionen.hs
@@ -3,4 +3,11 @@ import Term
 
 data Subst = Subst Term -> Term
 
+identity :: Subst
+
 single :: VarName -> Term -> Subst
+
+apply :: Subst -> Term -> Term
+apply (Subst s) = s
+
+compose :: Subst -> Subst -> Subst
