9d82567ae875d7e08c17426caa66cbeb793b9ed7
Niklas Carstensen <creeper99.mc@gmail.com>
2019-02-27 01:39:07 +0100

    Added sick single thingy

--- fortprog-prak-skeleton/src/Substitutionen.hs
+++ fortprog-prak-skeleton/src/Substitutionen.hs
@@ -7,6 +7,16 @@ identity :: Subst
 identity = Subst (\x -> x)
 
 single :: VarName -> Term -> Subst
+single y t = Subst $ replace y t
+  where
+      replace y t x = case x of 
+                        Var v -> if (v == y) 
+                                   then t
+                                   else Var v
+                        Comb c ts -> Comb c (map (replace y t) ts)
+
+identity :: Subst
+identity = Subst (\x -> x)
 
 apply :: Subst -> Term -> Term
 apply (Subst s) = s
