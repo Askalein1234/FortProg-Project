b67a524414cb63e75356a5208e1515560af0414b
Michel Thomsen <michel.thomsen@thomsen-online.de>
2019-03-01 10:51:01 +0100

    choose match implementation

--- fortprog-prak-skeleton/src/Matching.hs
+++ fortprog-prak-skeleton/src/Matching.hs
@@ -3,33 +3,12 @@ import Term
 import Substitutionen
 
 match :: Term -> Term -> Maybe Subst
-match (Var v) t               = Just (single v t)
-match (Comb n xs) (Var v)     = Nothing
-match (Comb n xs) (Comb m ys) = if (length xs) == (length ys) && 
-                                    notContainsNothing (map (\x -> match (xs!!x) (ys!!x)) [0..((length xs) - 1)])
-                                then Just (foldr (.) id (map (\x -> fromJust(match (xs!!x) (ys!!x))) [0..((length xs) - 1)]))
-                                else Nothing
-  where
-    notContainsNothing :: [Maybe Subst] -> Bool
-    notContainsNothing [] = True
-    notContainsNothing (x:xs) = case x of 
-                                  Just a  -> notContainsNothing xs
-                                  Nothing -> False
-
-    fromJust :: Maybe a -> a
-    fromJust (Just b) = b
-
-match1 :: Term -> Term -> Maybe Subst
-match1 (Var v)     t           = Just $ single v t
-match1 (Comb _ _)  (Var _)     = Nothing
-match1 (Comb _ xs) (Comb _ ys) = matchList xs ys
+match (Var v)     t           = Just $ single v t
+match (Comb _ _)  (Var _)     = Nothing
+match (Comb m xs) (Comb m ys) = if m == n then matchList xs ys else Nothing
     where
         matchList []     _      = Nothing
         matchList _      []     = Nothing
-        matchList (x:xs) (y:ys) = let rest = matchList xs ys
-                                      head = match x y in
-                                    case rest of 
-                                        Nothing -> head
-                                        Just t  -> case head of
-                                                    Nothing -> rest
-                                                    Just t' -> Just $ t' . t
+        matchList (x:xs) (y:ys) = case (matchList xs ys, match x y) of 
+                                    (Just t, Just t') -> Just $ t' . t
+                                    (_, _)            -> Nothing
