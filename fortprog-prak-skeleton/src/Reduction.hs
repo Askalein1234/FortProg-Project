20b5124e7ddcf09d9c0f2a146041728f34c50a21
Michel Thomsen <michel.thomsen@thomsen-online.de>
2019-03-02 14:52:35 +0100

    change filename of Reduktionsschritte.hs to Reduction.hs

--- fortprog-prak-skeleton/src/Reduction.hs
+++ fortprog-prak-skeleton/src/Reduction.hs
@@ -1,6 +1,18 @@
-module Reduction where
+module Reduction(findRule, reduceAt, reduciblePos, isNormalForm) where
 
 import Prog
-import Matching
+import Term
+import Substitutionen
+import Positionen
 
 findRule :: Prog -> Term -> Maybe (Rhs, Subst)
+findRule _ _ = Nothing
+
+reduceAt :: Prog -> Term -> Pos -> Maybe Term
+reduceAt _ _ _ = Nothing
+
+reduciblePos :: Prog -> Term -> [Pos]
+reduciblePos _ _ = []
+
+isNormalForm :: Prog -> Term -> Bool
+isNormalForm _ _ = False
