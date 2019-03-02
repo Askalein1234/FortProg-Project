736d41c9b7cc762dbc10fd64373e2bdfaf68ccb1
Niklas Carstensen <creeper99.mc@gmail.com>
2019-03-02 16:03:22 +0100

    5 done

--- fortprog-prak-skeleton/src/Helper.hs
+++ fortprog-prak-skeleton/src/Helper.hs
@@ -3,3 +3,14 @@ module Helper(fromJust) where
 fromJust :: Maybe a -> a
 fromJust (Just b) = b
 fromJust Nothing = error "GEHT NICHT!!! ANGEWACHSEN!!!"
+
+tupelFirst :: (a, b) -> a
+tupelFirst (x, y) = x
+
+isNothing :: Maybe a -> Bool
+isNothing (Just _)  = False
+isNothing Nothing   = True
+
+processMaybe :: Maybe a -> (a -> b) -> b -> b
+processMaybe (Just x) f _ = f x
+processMaybe Nothing  _ x = x
--- fortprog-prak-skeleton/src/Reduction.hs
+++ fortprog-prak-skeleton/src/Reduction.hs
@@ -5,9 +5,10 @@ import Term
 import Substitutionen
 import Matching
 import Helper
+import Positionen
 
 findRule :: Prog -> Term -> Maybe (Rhs, Subst)
-findRule (Prog [])   t = Nothing
+findRule (Prog [])     t = Nothing
 findRule (Prog (r:rs)) t = let try = tryRule r t in
                              case try of
                                Nothing -> findRule (Prog rs) t
@@ -19,3 +20,15 @@ findRule (Prog (r:rs)) t = let try = tryRule r t in
                                Nothing -> Nothing
                                Just _  -> Just (r, fromJust res)
                                
+reduceAt :: Prog -> Term -> Pos -> Maybe Term
+reduceAt p t x = let res = findRule p (selectAt t x) in
+                   case res of 
+                     Nothing -> Nothing
+                     Just _  -> Just (selectAt t x)
+                     
+reduciblePos :: Prog -> Term -> [Pos]
+reduciblePos p t = filter (\x -> not(isNothing(reduceAt p t x))) (allPos t)
+
+-- Definition 3.9 (Normalform) Ein Term t heißt in Normalform, falls es keinen Term t' gibt mit t ⇒ t', d. h. falls kein weiterer Reduktionsschritt möglich ist
+isNormalForm :: Prog -> Term -> Bool
+isNormalForm p t = isNothing(findRule p t)
