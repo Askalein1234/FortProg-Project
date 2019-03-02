377b65071775411ead713ad1c12edf87e0305a82
Niklas Carstensen <creeper99.mc@gmail.com>
2019-03-02 14:54:45 +0100

    matching is now within 80 comumns

--- fortprog-prak-skeleton/src/Matching.hs
+++ fortprog-prak-skeleton/src/Matching.hs
@@ -9,13 +9,17 @@ match (Comb _ _)  (Var _)     = Nothing
 match (Comb n xs) (Comb m ys) 
             | m == n          = matchList xs ys
             | otherwise       = Nothing
-                where
-                    matchList xs' ys' = if length xs' == length ys' && notContainsNothing (map (\x -> match (xs'!!x) (ys'!!x)) [0..(length xs' - 1)])
-                                            then Just $ foldr compose identity (map (\x -> fromJust (match (xs'!!x) (ys'!!x))) [0..(length xs' - 1)])
-                                            else Nothing
-                                            
-                    notContainsNothing :: [Maybe Subst] -> Bool
-                    notContainsNothing [] = True
-                    notContainsNothing (x:xs') = case x of 
-                                                    Just _  -> notContainsNothing xs'
-                                                    Nothing -> False
+  where
+    matchList xs' ys' = if length xs' == length ys' && 
+                notContainsNothing (map (\x -> match (xs'!!x) (ys'!!x)) 
+                [0..(length xs' - 1)])
+      then Just $ foldr compose identity 
+           (map (\x -> fromJust (match (xs'!!x) (ys'!!x))) 
+           [0..(length xs' - 1)])
+      else Nothing
+
+    notContainsNothing :: [Maybe Subst] -> Bool
+    notContainsNothing [] = True
+    notContainsNothing (x:xs') = case x of 
+                                   Just _  -> notContainsNothing xs'
+                                   Nothing -> False
