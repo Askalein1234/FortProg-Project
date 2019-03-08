9c0f00f33bb2e9ea2a179a861abbb95eff6598e9
Niklas Carstensen <creeper99.mc@gmail.com>
2019-03-08 01:46:36 +0100

    Recolored debug

--- fortprog-prak-skeleton/src/Interactive.hs
+++ fortprog-prak-skeleton/src/Interactive.hs
@@ -1,17 +1,16 @@
 module Interactive(startLoop) where
 
-import Data.List.Split
-import System.Console.ANSI
-import System.FilePath.Windows
-import System.IO
-
-import Parser
 import Prog
 import Term
-
+import Parser
 import Evaluation
+import PrettyPrinting()
+import System.FilePath.Windows
 import PrettyPrinting
 import Reduction
+import System.IO
+import System.Console.ANSI
+import Data.List.Split
 
 type ProgName = String
 type LastFileLoadCommand = String
@@ -19,53 +18,48 @@ type MarkForClose = Bool
 
 header :: String
 header = unlines
-  [ "                      _   _               _        _ _  "
-  , "   Welcome to        | | | |             | |      | | | "
-  , "  ___ _ __ ___   ___ | | | |__   __ _ ___| | _____| | | "
-  , " / __| '_ ` _ \\ / _ \\| | | '_ \\ / _` / __| |/ / _ \\ | | "
-  , " \\__ \\ | | | | | (_) | | | | | | (_| \\__ \\   <  __/ | | "
-  , " |___/_| |_| |_|\\___/|_| |_| |_|\\__,_|___/_|\\_\\___|_|_| "
-  , "                                                        "
-  , "                                     Type :h for help   "
-  ]
+  [ "                      _   _               _        _ _  ",
+    "   Welcome to        | | | |             | |      | | | ",
+    "  ___ _ __ ___   ___ | | | |__   __ _ ___| | _____| | | ",
+    " / __| '_ ` _ \\ / _ \\| | | '_ \\ / _` / __| |/ / _ \\ | | ",
+    " \\__ \\ | | | | | (_) | | | | | | (_| \\__ \\   <  __/ | | ",
+    " |___/_| |_| |_|\\___/|_| |_| |_|\\__,_|___/_|\\_\\___|_|_| ",
+    "                                                        ",
+    "                                     Type :h for help   " ]
   
 helpMessage :: String
 helpMessage = unlines 
-  [ "   ____                                          _        "
-  , "  / ___|___  _ __ ___  _ __ ___   __ _ _ __   __| |___     "
-  , " | |   / _ \\| '_ ` _ \\| '_ ` _ \\ / _` | '_ \\ / _` / __|    "
-  , " | |__| (_) | | | | | | | | | | | (_| | | | | (_| \\__ \\    "
-  , "  \\____\\___/|_| |_| |_|_| |_| |_|\\__,_|_| |_|\\__,_|___/    "
-  , "                                                           "
-  , "  <expression>       Evaluates the specified expression.   "
-  , "  :h[elp]            Shows this help message.              "
-  , "  :l[oad] <file>     Loads the specified file.             "
-  , "  :l[oad]            Unloads the currently loaded file.    "
-  , "  :r[eload]          Reloads the lastly loaded file.       "
-  , "  :s[et] <strategy>  Sets the specified evaluation strategy"
-  , "                     where <strategy> is one of 'lo', 'li',"
-  , "                     'ro', 'ri', 'po', or 'pi'.            "
-  , "  :q[uit]            Exits the interactive environment.    "
-  ]
+  [ "   ____                                          _        ",
+    "  / ___|___  _ __ ___  _ __ ___   __ _ _ __   __| |___     ",
+    " | |   / _ \\| '_ ` _ \\| '_ ` _ \\ / _` | '_ \\ / _` / __|    ",
+    " | |__| (_) | | | | | | | | | | | (_| | | | | (_| \\__ \\    ",
+    "  \\____\\___/|_| |_| |_|_| |_| |_|\\__,_|_| |_|\\__,_|___/    ",
+    "                                                           ",
+    "  <expression>       Evaluates the specified expression.   ",
+    "  :h[elp]            Shows this help message.              ",
+    "  :l[oad] <file>     Loads the specified file.             ",
+    "  :l[oad]            Unloads the currently loaded file.    ",
+    "  :r[eload]          Reloads the lastly loaded file.       ",
+    "  :s[et] <strategy>  Sets the specified evaluation strategy",
+    "                     where <strategy> is one of 'lo', 'li',",
+    "                     'ro', 'ri', 'po', or 'pi'.            ",
+    "  :q[uit]            Exits the interactive environment.    " ]
     
 quitMessage :: String
 quitMessage = unlines 
-  [ "                                        "
-  , "     ____                ____           "
-  , "    / __ )__  _____     / __ )__  _____ "
-  , "   / __  / / / / _ \\   / __  / / / / _ \\"
-  , "  / /_/ / /_/ /  __/  / /_/ / /_/ /  __/"
-  , " /_____/\\__, /\\___/  /_____/\\__, /\\___/ "
-  , "       /____/              /____/       "
-  , "                                        "
-  ]
+  [ "                                        ",
+    "     ____                ____           ",
+    "    / __ )__  _____     / __ )__  _____ ",
+    "   / __  / / / / _ \\   / __  / / / / _ \\",
+    "  / /_/ / /_/ /  __/  / /_/ / /_/ /  __/",
+    " /_____/\\__, /\\___/  /_____/\\__, /\\___/ ",
+    "       /____/              /____/       ", 
+    "                                        " ]
 
 startLoop :: IO ()
 startLoop = do 
   clearScreen 
-  setSGR [SetBlinkSpeed NoBlink]
   setSGR [SetColor Foreground Vivid Magenta]
-  setSGR [SetColor Background Dull Black]
   setTitle "Smol Haskell"
   putStr header
   inputLoop (Prog [], "", "", loStrategy, False)
@@ -74,8 +68,7 @@ startLoop = do
   setTitle "Ordinary Console Title"
   setSGR [Reset]
 
-inputLoop :: (Prog, ProgName, LastFileLoadCommand, Strategy, MarkForClose)
-          -> IO ()
+inputLoop :: (Prog, ProgName, LastFileLoadCommand, Strategy, MarkForClose) -> IO ()
 inputLoop (_, _, _, _, True)  = return ()
 inputLoop (p, n, l, s, False) = do 
   setSGR [SetColor Foreground Dull Cyan]
@@ -87,13 +80,8 @@ inputLoop (p, n, l, s, False) = do
   output <- processInput p n l s input
   inputLoop output
 
-processInput :: Prog -> ProgName -> LastFileLoadCommand -> Strategy -> String
-             -> IO ( Prog
-                   , ProgName
-                   , LastFileLoadCommand
-                   , Strategy
-                   , MarkForClose
-                   )
+processInput :: Prog -> ProgName -> LastFileLoadCommand -> Strategy -> String ->
+                  IO (Prog, ProgName, LastFileLoadCommand, Strategy, MarkForClose)
 processInput p n l s input 
   | input == ""        = do return (p, n, l, s, False)
   | input == ":h" || 
@@ -102,27 +90,22 @@ processInput p n l s input
   | input == ":q" || 
     input == ":quit"   = do return (p, n, l, s, True)
   | input == ":d" || 
-    input == ":debug"  = do setSGR [SetColor Foreground Vivid Blue]
-                            setSGR [SetColor Background  Vivid White]
-                            putStrLn ("Prog:\n" ++ (pretty p) ++ 
+    input == ":debug"  = do setSGR [SetColor Foreground Vivid Yellow]
+                            putStrLn ("Prog: " ++ (show p) ++ 
                                       "\nProgName: " ++ (show n) ++ 
                                       "\nLastFileLoadCommand: " ++ (show l))
-                            setSGR [SetColor Background Dull Black]
                             return (p, n, l, s, False)
-  | (words input)!!0 == ":d"
-                       = do setSGR [SetColor Foreground Vivid Blue]
-                            setSGR [SetColor Background  Vivid White]
-                            input' <- return (drop 3 input)
-                            _ <- case 
-                                   (parse::String -> Either String Term) input'
-                                   of Left m  -> do
-                                        setSGR [SetColor Foreground Vivid Red]
-                                        putStrLn m
-                                        return (p, n, l, s, False)
-                                      Right t -> do
-                                        debugEval s p t
-                                        return (p, n, l, s, False)
-                            setSGR [SetColor Background Dull Black]
+  | ((words input)!!0 == ":d" || 
+    (words input)!!0 == ":debug")
+                       = do setSGR [SetColor Foreground Vivid Yellow]
+                            input' <- return 
+                              (drop ((length ((words input)!!0)) + 1) input)
+                            case (parse::String -> Either String Term) input' of 
+                              Left m  -> do setSGR [SetColor Foreground Vivid Red]
+                                            putStrLn m
+                                            return (p, n, l, s, False)
+                              Right t -> do debugEval s p t
+                                            return (p, n, l, s, False)
                             return (p, n, l, s, False)
   | input == ":r" || 
     input == ":reload" = if l == ""
@@ -134,21 +117,19 @@ processInput p n l s input
   | input == ":l" || 
     input == ":load"   = do setTitle "Smol Haskell"
                             return (Prog [], "", l, s, False)
-  | ((words input)!!0 == ":l" ||
+  | ((words input)!!0 == ":l" || 
     (words input)!!0 == ":load") &&
     (words input)!!1 == "-d"
-                       = case
-                           (parse::String -> Either String Prog) 
-                           (drop 6 (unlines (splitOn ", " input)))
-                           of Left m  -> do 
-                                setSGR [SetColor Foreground Vivid Red]
-                                putStrLn m
-                                return (p, n, l, s, False)
-                              Right p' -> do
-                                setSGR [SetColor Foreground Vivid Green]
-                                putStrLn "Loaded program!"
-                                setTitle "Smol Haskell - Unnamed Program"
-                                return (p', n, l, s, False)
+                       = case (parse::String -> Either String Prog) 
+                           (drop (4 + length ((words input)!!0)) 
+                             (unlines (splitOn ", " input))) of 
+                           Left m  -> do setSGR [SetColor Foreground Vivid Red]
+                                         putStrLn m
+                                         return (p, n, l, s, False)
+                           Right p' -> do setSGR [SetColor Foreground Vivid Green]
+                                          putStrLn "Loaded program!"
+                                          setTitle "Smol Haskell - Unnamed Program"
+                                          return (p', n, l, s, False)
   | (words input)!!0 == ":l" ||
     (words input)!!0 == ":load"            
                        = do out <- loadFile p n l s input
@@ -163,32 +144,24 @@ processInput p n l s input
                            "ri" -> return (p, n, l, riStrategy, False)
                            "po" -> return (p, n, l, poStrategy, False)
                            "pi" -> return (p, n, l, piStrategy, False)
-                           _    -> do
-                             setSGR [SetColor Foreground Vivid Red]
-                             putStrLn "I don't know that evaluation strategy :/"
-                             return (p, n, l, s, False)
-  | otherwise          = case
-                           (parse::String -> Either String Term) input
-                           of Left m  -> do
-                                setSGR [SetColor Foreground Vivid Red]
-                                putStrLn m
+                           _    -> 
+                             do setSGR [SetColor Foreground Vivid Red]
+                                putStrLn "I don't know that evaluation strategy :/"
                                 return (p, n, l, s, False)
-                              Right t -> do
-                                setSGR [SetColor Foreground Vivid Green]
-                                putStrLn $ pretty $ evaluateWith s p t
-                                return (p, n, l, s, False)
+  | otherwise          = case (parse::String -> Either String Term) input of 
+                           Left m  -> do setSGR [SetColor Foreground Vivid Red]
+                                         putStrLn m
+                                         return (p, n, l, s, False)
+                           Right t -> do setSGR [SetColor Foreground Vivid Green]
+                                         putStrLn $ pretty $ evaluateWith s p t
+                                         return (p, n, l, s, False)
   where
-    loadFile :: Prog -> ProgName -> LastFileLoadCommand -> Strategy -> String 
-             -> IO ( Prog
-                   , ProgName
-                   , LastFileLoadCommand
-                   , Strategy
-                   , MarkForClose
-                   )
+    loadFile :: Prog -> ProgName -> LastFileLoadCommand -> Strategy -> String -> 
+      IO (Prog, ProgName, LastFileLoadCommand, Strategy, MarkForClose)
     loadFile p' n' l' s' input' = 
       do loadInput <- if elem '.' input' 
-                        then return (input') 
-                        else return (input' ++ ".smolhs")
+                      then return (input') 
+                      else return (input' ++ ".smolhs")
          parseOut <- (parseFile::FilePath -> 
            IO (Either String Prog)) ((words loadInput)!!1)
          case parseOut of 
@@ -204,14 +177,11 @@ processInput p n l s input
                             loadInput, s', False)
                             
     debugEval :: Strategy -> Prog -> Term -> IO ()
-    debugEval s' p' t = if (isNormalForm p' t)
-                      then do
-                        putStrLn $ "Normal Form: " ++ (pretty t)
-                        return ()
-                      else 
-                        case (reduceWith s' p' t) of
-                          Nothing -> return ()
-                          Just a  -> do
-                            putStrLn ("Reduction on pos: " ++ (show (s' p' t)))
-                            putStrLn $ pretty $ a
-                            debugEval s' p' a
+    debugEval s p t = if (isNormalForm p t)
+                      then do putStrLn $ "Normal Form: " ++ (pretty t)
+                              return ()
+                      else case (reduceWith s p t) of
+                             Nothing -> return ()
+                             Just a  -> do putStrLn ("Reduction on pos: " ++ (show (s p t)))
+                                           putStrLn $ pretty $ a
+                                           debugEval s p a
